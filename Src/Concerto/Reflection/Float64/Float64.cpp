a2445e7843aecd94c99e85d3617d373ff566b897
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2026-05-10 10:58:11 +0200

    reflection: add ToString method to Float64 class for string representation

--- Src/Concerto/Reflection/Float64/Float64.cpp
+++ Src/Concerto/Reflection/Float64/Float64.cpp
@@ -1,3 +1,6 @@
+#include <iomanip>
+#include <sstream>
+#include <string>
 #include "Concerto/Reflection/Float64/Float64.refl.hpp"
 
 namespace cct::refl
@@ -43,4 +46,17 @@ namespace cct::refl
 	bool Float64::operator!=(const Float64& other) const { return m_value != other.m_value; }
 	bool Float64::operator==(cct::Float64 other) const { return m_value == other; }
 	bool Float64::operator!=(cct::Float64 other) const { return m_value != other; }
+
+	std::string Float64::ToString() const
+	{
+		std::ostringstream oss;
+		oss << std::fixed << std::setprecision(15) << m_value;
+		std::string s = oss.str();
+		s.erase(s.find_last_not_of('0') + 1);
+		if (s.back() == '.')
+		{
+			s.pop_back();
+		}
+		return s;
+	}
 } // namespace cct::refl
--- Src/Concerto/Reflection/Float64/Float64.refl.hpp
+++ Src/Concerto/Reflection/Float64/Float64.refl.hpp
@@ -32,6 +32,8 @@ namespace cct::refl
 		[[nodiscard]] bool operator==(cct::Float64 other) const;
 		[[nodiscard]] bool operator!=(cct::Float64 other) const;
 
+		[[nodiscard]] std::string ToString() const override;
+
 		CCT_OBJECT(Float64);
 
 	private:
